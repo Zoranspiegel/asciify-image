265337e05457df32691ecf2cf14d0811d0182d38
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-21 17:59:12 -0500

    new cloudinary folder

--- api/src/services/uploadToCloudinary.service.js
+++ api/src/services/uploadToCloudinary.service.js
@@ -5,12 +5,12 @@ const uploadToCloudinary = async (image, name) => {
   // SUBIR IMAGEN ASCII A CLOUDINARY
   const data = await cloudinary.uploader.upload(image, {
     public_id: name,
-    folder: 'ASCII Images',
+    folder: 'dark-secrets/ascii-avatars',
     // width: 1920,
     // crop: 'scale',
     // quality: 'auto:best',
     // fetch_format: 'auto'
-  })
+  });
 
   // RETORNAR URL SEGURA DE LA IMAGEN ALOJADA EN CLOUDINARY
   return data.secure_url;
